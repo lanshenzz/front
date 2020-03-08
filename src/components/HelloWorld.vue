5e42851692c6410d8b39b62098b98f0ea84b9eb3
lanshenzz <495102181@qq.com>
2020-03-08 16:11:26 +0800

    知识图谱前端第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <div v-for="(item,index) in userInfo" :key="index">
+      <span>{{item.id}}</span>
+      <span>{{item.name}}</span>
+      <span>{{item.age}}</span>
+      <span>{{item.sex}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      userInfo:null
+    }
+  },
+  created(){
+    this.getUser()
+  },
+  methods: {
+
+    getUser(){
+      this.$axios.get('/user/getAll').then(res=>{
+        // console.log(res.data);
+        this.userInfo=res.data
+      })
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
